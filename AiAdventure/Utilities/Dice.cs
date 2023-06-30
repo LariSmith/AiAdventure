3593ce51e53380b436010ce22988b417052b900f
YouSayLari <larissa@think4.com.br>
2023-06-30 14:33:13 -0300

    Creating utility Dice

---
+++ AiAdventure/Utilities/Dice.cs
@@ -0,0 +1,12 @@
+﻿namespace AiAdventure.Utilities
+{
+    public class Dice
+    {
+        private static Random _random = new Random();
+
+        public static int Roll(int sides)
+        {
+            return _random.Next(1, sides + 1);
+        }
+    }
+}
