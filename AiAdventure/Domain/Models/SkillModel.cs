94fae0140d07e0f1bceb3a73c1ae939e0733aefb
YouSayLari <larissa@think4.com.br>
2023-06-30 14:32:05 -0300

    Creating skill and turn models

---
+++ AiAdventure/Domain/Models/SkillModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AiAdventure.Domain.Models
+{
+    public class SkillModel
+    {
+        public string Description { get; private set; }
+        public int Points { get; private set; }
+    }
+}
---
+++ AiAdventure/Domain/Models/TurnModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AiAdventure.Domain.Models
+{
+    public class TurnModel
+    {
+        public int Number { get; set; }
+        public string Weather { get; set; }
+        public string Scene { get; set; }
+        public int CurrentDay { get; set; }
+        public string PeriodDay { get; set; }
+    }
+}
