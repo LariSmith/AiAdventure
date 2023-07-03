1fa96e1864c1996b2d294f279d3984534e79a4f8
YouSayLari <larissa@think4.com.br>
2023-07-03 17:26:30 -0300

    Adding annotation in GameController

--- AiAdventure/Controllers/GameController.cs
+++ AiAdventure/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using AiAdventure.Interfaces.Services;
+﻿using AiAdventure.DTOs;
+using AiAdventure.Interfaces.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,11 +20,18 @@ namespace AiAdventure.Controllers
 
         [HttpPost]
         [Route("new-game")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CharacterDto))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
         public async Task<IActionResult> StartGame([FromForm]bool testOnly = false)
         {
             try
             {
                 var playerId = int.Parse(User.FindFirst("sid")?.Value);
+
+                if (playerId == null)
+                    return Unauthorized();
+
                 var model = await _gameService.CreateNewGame(playerId, testOnly);
                 
                 return Ok(model);
