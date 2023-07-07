cbf0f21426573d8dafc277bad4ba07cee501a772
YouSayLari <larissa@think4.com.br>
2023-07-07 16:31:51 -0300

    Making new migration

--- AiAdventure/Migrations/20230703215052_CreateGameLog.Designer.cs
+++ AiAdventure/Migrations/20230703215052_CreateGameLog.Designer.cs
@@ -4,6 +4,7 @@ using AiAdventure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AiAdventure.Migrations
 {
     [DbContext(typeof(AiAdventureContext))]
-    partial class AiAdventureContextModelSnapshot : ModelSnapshot
+    [Migration("20230703215052_CreateGameLog")]
+    partial class CreateGameLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AiAdventure/Migrations/20230703215052_CreateGameLog.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AiAdventure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGameLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Json",
+                table: "turns",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Json",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "gameLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gameLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_gameLogs_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_gameLogs_CharacterId",
+                table: "gameLogs",
+                column: "CharacterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gameLogs");
+
+            migrationBuilder.DropColumn(
+                name: "Json",
+                table: "turns");
+
+            migrationBuilder.DropColumn(
+                name: "Json",
+                table: "Characters");
+        }
+    }
+}
